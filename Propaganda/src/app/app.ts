91f37d33019d42c1a95db91fb8975e6830e2fece
unknown <semonsaleeb@gmail.com  git config --global user.name semongit config --global user.email semonsaleeb@gmail.com>
2025-07-03 18:01:08 +0300

    section3

--- Propaganda/src/app/app.ts
+++ Propaganda/src/app/app.ts
@@ -4,10 +4,11 @@ import { Header } from './header/header';
 import { Hero } from './home/hero/hero';
 import { CommonModule } from '@angular/common';
 import { Workspace } from './home/workspace/workspace';
+import { UsersAndGroups } from './home/users-and-groups/users-and-groups';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, Header, Hero, Workspace],
+  imports: [CommonModule, RouterOutlet, Header, Hero, Workspace, UsersAndGroups],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ Propaganda/src/app/home/users-and-groups/users-and-groups.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-and-groups',
+  imports: [],
+  templateUrl: './users-and-groups.html',
+  styleUrl: './users-and-groups.scss'
+})
+export class UsersAndGroups {
+
+}
